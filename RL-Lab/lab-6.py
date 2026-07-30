80b4a9b71c74d83d5115e86e6d8d0276b88a74d6
192472371 <192472371.simats@saveetha.com>
2026-07-30 10:49:53 +0530

    Added lab files

--- RL-Lab/lab-6.py
+++ RL-Lab/lab-6.py
@@ -1,14 +1,14 @@
-import gym
+import gymnasium as gym
 
-env = gym.make("FrozenLake-v1", render_mode=None)
+env = gym.make("FrozenLake-v1")
 
-state, _ = env.reset()
+state, info = env.reset()
 done = False
 total_reward = 0
 
 while not done:
-    action = env.action_space.sample()   # Random action
-    state, reward, terminated, truncated, _ = env.step(action)
+    action = env.action_space.sample()
+    state, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
     total_reward += reward
 
